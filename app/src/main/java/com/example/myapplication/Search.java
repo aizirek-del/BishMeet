8b336c28b7f61f47e6a41b17c339e3803a1177a5
kunduz51 <kunduz.kerimova.98@mail.ru>
2021-10-26 19:33:34 +0600

    fix problems2

--- app/src/main/java/com/example/myapplication/Search.java
+++ app/src/main/java/com/example/myapplication/Search.java
@@ -1,6 +1,10 @@
 package com.example.myapplication;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.annotation.SuppressLint;
 import android.app.DatePickerDialog;
@@ -12,17 +16,37 @@ import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.ImageView;
+import android.widget.ProgressBar;
 import android.widget.SearchView;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.firebase.database.ChildEventListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
 
+import java.util.ArrayList;
 import java.util.Calendar;
+import java.util.List;
 
 public class Search extends AppCompatActivity {
-    ImageView mdateformat2;
     int year;
     int month;
     int day;
+    ImageView mdateformat2;
+    private final List<NewGroupData> mItems = new ArrayList<>();
+    GroupsAdapter mAdapter;
+    RecyclerView mRecyclerView;
+    private ProgressBar progressBar;
+    private final List<NewEvent> eventList = new ArrayList<>();
+    RecyclerView verticalRecView;
+    EventAdapter eventAdapter;
+    DatabaseReference mDatareference;
+    ProgressBar progress;
+    private DatabaseReference databaseReference;
+
 
     @SuppressLint("WrongViewCast")
     @Override
@@ -30,9 +54,96 @@ public class Search extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search);
 
+        databaseReference = FirebaseDatabase.getInstance().getReference("groups");
+
+
+        databaseReference.addChildEventListener(new ChildEventListener() {
+            @Override
+            public void onChildAdded(@NonNull DataSnapshot snapshot, @Nullable String previousChildName) {
+
+                NewGroupData newData = snapshot.getValue(NewGroupData.class);
+                mItems.add(newData);
+                initRecycler(mItems);
+                progressBar.setVisibility(View.INVISIBLE);
+            }
+
+            private void initRecycler(List<NewGroupData> list) {
+                mAdapter = new GroupsAdapter(Search.this, list);
+                mRecyclerView.setAdapter(mAdapter);
+
+            }
+            @Override
+            public void onChildChanged(@NonNull DataSnapshot snapshot, @Nullable String previousChildName) {
+
+                NewGroupData datas = snapshot.getValue(NewGroupData.class);
+                String dataKey = snapshot.getKey();
+
+            }
+
+            @Override
+            public void onChildRemoved(@NonNull DataSnapshot snapshot) {
+                String dataKey = snapshot.getKey();
+            }
+
+            @Override
+            public void onChildMoved(@NonNull DataSnapshot snapshot, @Nullable String previousChildName) {
+                NewGroupData data = snapshot.getValue(NewGroupData.class);
+                String dataKey = snapshot.getKey();
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+                progressBar.setVisibility(View.INVISIBLE);
+            }
+
+        });
+
+
+      //  verticalRecView = view.findViewById(R.id.vertical_recycler_view);
+        mDatareference = FirebaseDatabase.getInstance().getReference("events");
+
+        mDatareference.addChildEventListener(new ChildEventListener() {
+            @Override
+            public void onChildAdded(@NonNull DataSnapshot snapshot, @Nullable String previousChildName) {
+                NewEvent events = snapshot.getValue(NewEvent.class);
+                eventList.add(events);
+                initEventRecycler(eventList);
+                progress.setVisibility(View.GONE);
+            }
+            private void initEventRecycler(List<NewEvent> eList) {
+                eventAdapter = new EventAdapter(Search.this, eList);
+                verticalRecView.setAdapter(eventAdapter);
+            }
+
+            @Override
+            public void onChildChanged(@NonNull DataSnapshot snapshot, @Nullable String previousChildName) {
+                NewEvent event = snapshot.getValue(NewEvent.class);
+                String key = snapshot.getKey();
+
+            }
+
+            @Override
+            public void onChildRemoved(@NonNull DataSnapshot snapshot) {
+
+                String key = snapshot.getKey();
+            }
+
+            @Override
+            public void onChildMoved(@NonNull DataSnapshot snapshot, @Nullable String previousChildName) {
+                NewEvent event = snapshot.getValue(NewEvent.class);
+                String key = snapshot.getKey();
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+                progress.setVisibility(View.INVISIBLE);
+
+            }
+        });
+
+
         mdateformat2 = findViewById(R.id.calendar);
-
-
 
         Calendar calendar = Calendar.getInstance();
         mdateformat2.setOnClickListener(new View.OnClickListener() {
@@ -46,8 +157,6 @@ public class Search extends AppCompatActivity {
                     @Override
                     public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
                         String sDate = dayOfMonth + "." + month + "." + year;
-                        //  mdateformat.setText(SimpleDateFormat.getDateInstance().format(calendar.getTime()));
-                        //mdateformat2.setText(sDate);
 
                     }
                 }, year,month,day);
--- app/src/main/java/com/example/myapplication/SearchActivity.java
+++ app/src/main/java/com/example/myapplication/SearchActivity.java
@@ -10,5 +10,7 @@ public class SearchActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search);
+
+
     }
 }
